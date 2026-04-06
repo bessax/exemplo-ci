20e095bb6e1a470f029097f26185cfb5ea45a89c
bessax <andrebessax@gmail.com>
2026-04-06 20:19:01 -0300

    refactor: ajuste na mensagem da API.feat: alteração no program.cs

--- my-app-example-ci/Program.cs
+++ my-app-example-ci/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Endpoint raiz
-app.MapGet("/", () => "Olá, DevOps com .NET!");
+app.MapGet("/", () => $"Olá turma de sistemas, DevOps com .NET! {DateTime.UtcNow}");
 
 // Endpoint de status
 app.MapGet("/status", () => new 
